0059723b6b309b0f41790db6d94da43ca908e8ea
amir <amirtaki623@gmail.com>
2025-08-01 23:32:58 +0330

    js part 3 project

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
 let celsius = document.getElementById("celsius")
 let fahrenheit = document.getElementById("fahrenheit")
+  
+
+const celsiusToFahrenheit = () => {
+    const output = (parseFloat(celsius.value) * 9) / 5 + 32;
+}
